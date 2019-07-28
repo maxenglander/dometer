8a2e6842f4d08bf975dd329f0a20197f9623895e
Max Englander <max.englander@gmail.com>
2019-07-28 17:43:00 -0400

    start reworking main.cc as domain socket

--- main/main.cc
+++ main/main.cc
@@ -1,17 +1,45 @@
+#include <cstdio>
+#include <cstdlib>
 #include <cstring>
-#include <netinet/in.h>
+#include <errno.h>
 #include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
 
-#define SERV_PORT 53
+#define DEFAULT_SOCK_PATH "/var/run/dns-telemeter.sock"
 
 int
 main(int argc, char **argv) {
-    struct sockaddr_in servaddr;
-    int sockfd;
+    struct sockaddr_un addr;
+    int rc, sockfd;
 
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    memset(&servaddr, sizeof(servaddr), 0);
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(SERV_PORT);
+    memset(&addr, 0, sizeof(addr));
+
+    /******************************************/
+    /* Create a UNIX domain stream socket.    */
+    /******************************************/
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if(sockfd == -1) {
+        fprintf(stderr, "Failed to create unix domain socket: %s (%d)\n", strerror(errno), errno);
+        exit(1);
+    }
+
+    /******************************************/
+    /* Set up the UNIX sockaddr structure.    */
+    /* by using AF_UNIX for the family and    */
+    /* giving it a filepath to bind to.       */
+    /*                                        */
+    /* Unlink the file so that the bind will  */
+    /* succeed, then bind to that file.       */
+    /******************************************/
+    addr.sun_family = AF_INET;
+    strncat(addr.sun_path, DEFAULT_SOCK_PATH, sizeof(addr.sun_path) - 1);
+
+    unlink(DEFAULT_SOCK_PATH);
+    rc = bind(sockfd, (struct sockaddr *) &addr, sizeof(addr));
+    if(rc == -1) {
+        fprintf(stderr, "Failed to bind socket: %s (%d)\n", strerror(errno), errno);
+        close(sockfd);
+        exit(1);
+    }
 }
