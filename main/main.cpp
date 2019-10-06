7f42afe46f260c3ea3746dea82d7c14f234c7b98
Max Englander <max.englander@gmail.com>
2019-10-05 20:19:30 -0400

    checkpoint

--- main/main.cpp
+++ main/main.cpp
@@ -1,18 +1,24 @@
 #include <iostream>
+#include <memory>
 
 #include "experimental/expected.hpp"
+#include "network/dns/server/event_type.hpp"
+#include "network/dns/server/native_resolving_handler.hpp"
 #include "network/dns/server/server.hpp"
 
 using namespace Dometer::Network;
 using namespace std::experimental;
 
 int main(int argc, char **argv) {
-    Dns::Server::Server server;
+    auto handler = std::make_unique<Dns::Server::NativeResolvingHandler>();
+    Dns::Server::Server server(std::move(handler));
     auto result = server.serve();
+
     if(!result) {
         std::cerr << "failed to start DNS server: " + result.error().message
             + " (" + std::to_string(result.error().number) + ")" << std::endl;
         return 1;
     }
+
     return 0;
 }
