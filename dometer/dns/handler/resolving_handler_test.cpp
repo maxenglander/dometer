92c01f1b4e1fa6f79c540909e4e4c729c67fb534
Max Englander <max.englander@gmail.com>
2020-03-22 01:02:03 -0400

    yayayaya

--- dometer/dns/handler/resolving_handler_test.cpp
+++ dometer/dns/handler/resolving_handler_test.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <stdexcept>
 
 #include "dometer/dns/class.hpp"
 #include "dometer/dns/opcode.hpp"
@@ -7,15 +8,19 @@
 #include "dometer/dns/type.hpp"
 #include "dometer/dns/event/any_event.hpp"
 #include "dometer/dns/event/parse_query_event.hpp"
+#include "dometer/dns/event/resolve_query_event.hpp"
 #include "dometer/dns/handler/resolving_handler.hpp"
 #include "dometer/dns/message/builder.hpp"
 #include "dometer/dns/message/parser.hpp"
+#include "dometer/dns/resolver/error.hpp"
 #include "dometer/dns/resolver/error_code.hpp"
 #include "dometer/dns/resolver/mock_resolver.hpp"
 #include "dometer/event/mock_emitter.hpp"
 #include "gtest/gtest.h"
 
 using ::testing::_;
+using ::testing::AnyNumber;
+using ::testing::Return;
 using ::testing::VariantWith;
 
 namespace dometer::dns::handler {
@@ -34,12 +39,19 @@ namespace dometer::dns::handler {
 
     TEST_F(ResolvingHandlerTest, EmitsAParseQueryEvent) {
         auto message = dometer::dns::message::builder::new_builder()
+            .add_question(dometer::dns::question{
+                "hello.world", dometer::dns::type::a, dometer::dns::class_::in
+            })
             .set_id(54321)
+            .set_opcode(dometer::dns::opcode::query)
+            .set_qr(dometer::dns::qr::query)
             .build();
         ASSERT_TRUE(message) << "Failed to create test message.";
 
-        EXPECT_CALL(*_emitter, emit(VariantWith<dometer::dns::event::parse_query_event>(_)));
+        /* Set up catch-all */
+        EXPECT_CALL(*_emitter, emit(_)).Times(AnyNumber());
 
+        EXPECT_CALL(*_emitter, emit(VariantWith<dometer::dns::event::parse_query_event>(_))).Times(1);
         _resolving_handler.handle(0, std::vector<uint8_t>((uint8_t*)*message, (uint8_t*)*message + message->size()));
     }
 
@@ -52,7 +64,7 @@ namespace dometer::dns::handler {
     TEST_F(ResolvingHandlerTest, ResolvesQueryIfValid) {
         auto query = dometer::dns::message::builder::new_builder()
             .add_question(dometer::dns::question{
-                "google.com", dometer::dns::type::a, dometer::dns::class_::in
+                "hello.world", dometer::dns::type::a, dometer::dns::class_::in
             })
             .set_id(54321)
             .build();
@@ -61,7 +73,36 @@ namespace dometer::dns::handler {
         _resolving_handler.handle(0, std::vector<uint8_t>((uint8_t*)*query, (uint8_t*)*query + query->size()));
     }
 
-    TEST_F(ResolvingHandlerTest, DoesNotResoleQueryIfInvalid) {
+    TEST_F(ResolvingHandlerTest, EmitsAResolveQueryEvent) {
+        auto message = dometer::dns::message::builder::new_builder()
+            .add_question(dometer::dns::question{
+                "hello.world", dometer::dns::type::a, dometer::dns::class_::in
+            })
+            .set_id(54321)
+            .set_opcode(dometer::dns::opcode::query)
+            .set_qr(dometer::dns::qr::query)
+            .build();
+
+        ASSERT_TRUE(message) << "Failed to create test message.";
+
+        /* Set up catch-all */
+        EXPECT_CALL(*_emitter, emit).Times(AnyNumber());
+
+        EXPECT_CALL(*_emitter, emit(VariantWith<dometer::dns::event::parse_query_event>(_))).Times(1);
+        _resolving_handler.handle(0, std::vector<uint8_t>((uint8_t*)*message, (uint8_t*)*message + message->size()));
+    }
+
+    TEST_F(ResolvingHandlerTest, DoesNotResovleQueryIfInvalid) {
+        auto query = dometer::dns::message::builder::new_builder()
+            .add_question(dometer::dns::question{
+                "hello.world", dometer::dns::type::a, dometer::dns::class_::in
+            })
+            .set_id(54321)
+            .set_opcode(dometer::dns::opcode::iquery)
+            .build();
+
+        EXPECT_CALL(*_resolver, resolve).Times(0);
+        _resolving_handler.handle(0, std::vector<uint8_t>((uint8_t*)*query, (uint8_t*)*query + query->size()));
     }
 
     TEST_F(ResolvingHandlerTest, ReturnsNotImplIfUnsupportedOpcode) {
