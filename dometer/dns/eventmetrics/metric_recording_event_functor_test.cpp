5a73db8c443d0a6bea020a8e065c2edbfe3d5e43
Max Englander <max.englander@gmail.com>
2020-03-24 00:02:51 -0400

    checkpoint

--- dometer/dns/eventmetrics/metric_recording_event_functor_test.cpp
+++ dometer/dns/eventmetrics/metric_recording_event_functor_test.cpp
@@ -1,4 +1,5 @@
 #include "dometer/dns/eventmetrics/metric_recording_event_functor.hpp"
 
 namespace dometer::dns::eventmetrics {
+ 
 }
---
+++ dometer/metrics/mock_recorder.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "dometer/metrics/recorder.hpp"
+#include "gtest/gmock.h"
+
+namespace dometer::metrics {
+    class mock_recorder : public recorder {
+        public:
+    };
+}
